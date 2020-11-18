285d9a6c7921048dce795542aff1235689c7ac99
StarWix <starwix.net@gmail.com>
2020-11-18 18:13:29 +0300

    Add auto sign in after sign up

--- src/app/outer/registration/registration.component.ts
+++ src/app/outer/registration/registration.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import {AuthService} from '../../services/auth.service';
 import {UserAgreementDialogComponent} from '../user-agreement-dialog/user-agreement-dialog.component';
+import {Router} from '@angular/router';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-registration',
@@ -15,7 +17,9 @@ export class RegistrationComponent implements OnInit {
   sending: boolean;
 
   constructor(public dialog: MatDialog,
-              private authService: AuthService) {
+              private authService: AuthService,
+              private router: Router,
+              private snackBar: MatSnackBar) {
   }
 
   ngOnInit(): void {
@@ -28,9 +32,28 @@ export class RegistrationComponent implements OnInit {
   }
 
   create() {
+    if (this.password.length < 7) {
+      this.snackBar.open('Минимальная длина пароля 7 символов.', undefined, {
+        duration: 5000
+      });
+      return;
+    }
     this.sending = true;
-    this.authService.create(this.name, this.email, this.password).subscribe(data => {
-        this.sending = false;
+    this.authService.create(this.name, this.email, this.password).subscribe(() => {
+        this.authService.login(this.email, this.password).subscribe(() => {
+          this.router.navigate(['/dashboard']);
+        }, () => {
+          this.sending = false;
+        });
+        this.snackBar.open('Регистрация завершена. Приятного обучения.', undefined, {
+          duration: 5000
+        });
+        this.password = '';
+    }, error => {
+      this.snackBar.open(error.error.message, undefined, {
+        duration: 5000
+      });
+      this.sending = false;
     });
   }
 }
